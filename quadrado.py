30484ba14820ea0bdb5d710fc0e49d337a35a7f1
LeonardoGuerrini <90059757+LeonardoGuerrini@users.noreply.github.com>
2021-11-25 21:07:15 -0300

    Update quadrado.py

--- quadrado.py
+++ quadrado.py
@@ -3,14 +3,21 @@ class Quadrado:
     def quadradox():
         while True:
             try:
-                print("Área do quadrado: A = L²")
-                lado = int(input("Informe o lado do quadrado: "))
-                print("Resultado:", lado * lado)
 
-                repetir = input("Você deseja fazer novamente? (sim/nao): ")
+                print("Área do quadrado: A = L²")
+
+                lado = int(input("Informe o lado do quadrado: "))
+                print("\nResultado:", lado * lado) # Resultado
+                    
+                print("")
+
+                print(f"Operação:\nA = L²\nA = {lado}²\nA = {lado}*{lado}\nA =",lado*lado)
+
+                repetir = str(input("\nVocê deseja fazer novamente? (sim/nao): "))
                 if repetir.lower() == "nao":
                     print("\nObrigado por usar o programa! :)")
-                    exit()
 
-            except ValueError:
-                print("Erro! O programa aceita apenas números!")
+                    break
+
+            except:
+                print("\nErro!")
