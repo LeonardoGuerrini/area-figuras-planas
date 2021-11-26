587bca0ec3e8a2d5a43e37a716059bc01a77d36a
LeonardoGuerrini <90059757+LeonardoGuerrini@users.noreply.github.com>
2021-11-25 21:06:29 -0300

    Update losango.py

--- losango.py
+++ losango.py
@@ -3,15 +3,22 @@ class Losango:
     def losangox():
         while True:
             try:
-                print("Área do losango: A = (D+d) / 2")
-                diagmaior = int(input("Informe a diagonal maior: "))
-                diagmenor = int(input("Informe a diagonal menor: "))
-                print("Resultado: ", (diagmaior * diagmenor) / 2)
 
-                repetir = input("Você deseja fazer novamente? (sim/nao): ")
+                print("Área do losângo: A =  D * d / 2")
+
+                diagmaior = int(input("Informe o valor da diagonal maior: "))
+                diagmenor = int(input("Informe o valor da diagonal menor: "))
+                print("Resultado:", (diagmaior * diagmenor) / 2)
+
+                print("")
+
+                print(f"Operação:\nA = D * d / 2\nA = {diagmaior} * {diagmenor} / 2\nA =",(diagmaior*diagmenor) / 2)
+
+                repetir = str(input("Você deseja fazer novamente? (sim/nao): "))
                 if repetir.lower() == "nao":
                     print("\nObrigado por usar o programa! :)")
-                    exit()
+                    
+                    break
 
-            except ValueError:
-                print("Erro! O programa aceita apenas números!")
+            except:
+                print("\nErro!")
