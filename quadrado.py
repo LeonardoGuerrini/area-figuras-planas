a9466428141a493beb74315655f19b4a01456a7d
LeonardoGuerrini <90059757+LeonardoGuerrini@users.noreply.github.com>
2021-11-25 21:44:06 -0300

    Update quadrado.py

--- quadrado.py
+++ quadrado.py
@@ -7,15 +7,18 @@ class Quadrado:
                 print("Área do quadrado: A = L²")
 
                 lado = int(input("Informe o lado do quadrado: "))
-                print("\nResultado:", lado * lado) # Resultado
-                    
+                print("\nResultado:", lado * lado)  # Resultado
+
                 print("")
 
-                print(f"Operação:\nA = L²\nA = {lado}²\nA = {lado}*{lado}\nA =",lado*lado)
+                print(
+                    f"Operação:\nA = L²\nA = {lado}²\nA = {lado}*{lado}\nA =", lado*lado)
 
-                repetir = str(input("\nVocê deseja fazer novamente? (sim/nao): "))
-                if repetir.lower() == "nao":
-                    print("\nObrigado por usar o programa! :)")
+                if str(input("Deseja continuar (sim/nao):")).lower() in ['s', 'sim']:
+                    print("\nContinuando...\n")
+
+                else:
+                    print("Obrigado por usar o programa! :)")
 
                     break
 
