5a7ec25ab138ac4a3a87c35fa2712403b4766f39
LeonardoGuerrini <90059757+LeonardoGuerrini@users.noreply.github.com>
2021-11-25 21:06:16 -0300

    Update circulo.py

--- circulo.py
+++ circulo.py
@@ -3,14 +3,22 @@ class Circulo:
     def circulox():
         while True:
             try:
-                print("Área do círculo: A = Pi * r²")
-                raio = int(input("Informe o raio do círculo: "))
-                print("Resultado:", 3.14 * (raio * raio))
 
-                repetir = input("Você deseja fazer novamente? (sim/nao): ")
+                print("Área do círculo: A = Pi * r²")
+
+                pi = 3.14
+                raio = int(input("Informe o valor do raio: "))
+                print("Resultado:", pi * (raio * raio))
+
+                print("")
+
+                print(f"Operação:\nA = Pi * r²\nA = {pi} * {raio}²\nA =", pi * (raio * raio))
+
+                repetir = str(input("Você deseja repetir? (sim/nao): "))
                 if repetir.lower() == "nao":
                     print("\nObrigado por usar o programa! :)")
-                    exit()
 
-            except ValueError:
-                print("Erro! O programa aceita apenas números!")
+                    break
+
+            except:
+                print("\nErro!")
