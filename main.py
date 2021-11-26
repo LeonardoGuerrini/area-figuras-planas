6579d1937b499f53dd83a80f0951e0730939188e
LeonardoGuerrini <90059757+LeonardoGuerrini@users.noreply.github.com>
2021-11-25 21:06:45 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 # Ficarei feliz em receber dicas :)
 from opcoes import Opcoes
 
-print("********************************")
-print("Calcule a área de figuras planas")
-print("********************************\n")
 Opcoes.opcoesx()
